c638e053a96f93eb94276981a879f2d809004001
tokarevartem124-glitch <tokarevartem124@gmail.com>
2026-09-18 13:03:40 +0500

    Add safe motion bootstrap

---
+++ v2-motion-boot.js
@@ -0,0 +1,1 @@
+(()=>{try{if(typeof io!=='undefined'&&io&&typeof io.observe==='function'){document.documentElement.classList.add('motion-ok')}}catch(e){document.documentElement.classList.remove('motion-ok')}})();
